ac2028d53ceaf8ca6c9fdce37a3cd3cb430f9a30
Brad Babines <bbabines@gmail.com>
2022-06-16 22:56:58 -0400

    Working on getting first and second number set plus the operator for the math fxn

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let calcAdd = (a, b) => {
     return a / b;
   };
   
-  let operate = (operator, num1, num2) => {
+  // switched operator and num1 in arguments but didnt check results
+  let operate = (num1, operator, num2) => {
     if (operator === "*") {
         return calcMultiply(num1, num2);
         } if (operator === "/") {
@@ -28,37 +29,69 @@ let calcAdd = (a, b) => {
    
   };
 
-let zero = document.getElementById("zero");
-let one = document.getElementById("one");
-let two = document.getElementById("two");
-let three = document.getElementById("three");
 
 let contents = document.getElementById("contents");
-let displayNum = contents.textContent;
+let calcBody = document.getElementById("calc-body");
+let buttonBody = document.getElementsByClassName("button-body");
+let operator = document.getElementsByClassName("operator");
 
-zero.addEventListener("click", () => {
-    contents.style.fontSize = "30px";
-    contents.style.marginRight = "10px";
-    contents.textContent = "0";
+
+
+//  Buttons work but operators and edges need to be excluded
+calcBody.addEventListener("click", e => {
+    let displayNum = contents.textContent;
+    let target = e.target.textContent;
+        // console.log(target);
+    let key = e.target;
+    let action = key.dataset.action;
+
+    //  Only add works for the operators...
+     if (
+    key === operator[0] || 
+    key === operator[1] || 
+    key === operator[2] || 
+    key === operator[3] || 
+    key === operator[4] || 
+    displayNum === "0" ||
+    displayNum === "+" 
+    // action === "divide"
+    ) {
+      contents.textContent = key.textContent;
+    } else {
+        contents.textContent = displayNum + key.textContent; 
+    // firstNumberSet working as intended.
+    // let firstNumberSet = displayNum + key.textContent;
+    // console.log(firstNumberSet);
+    }
+
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "divide" ||
+      action === "multiply" 
+    ) {
+      // Works
+      key.dataset.firstNumberSet = displayNum;
+      let firstNumberSet = key.dataset.firstNumberSet;
+      console.log(firstNumberSet + " is first number set");
+      // Works
+      key.dataset.operator = action;
+      let operator = key.dataset.operator;
+          console.log(operator + " is the operator")
+      
+      // Works
+    } else if (action === "calculate") {
+      let secondNumberSet = displayNum;
+          console.log(secondNumberSet + " is second number set");
+      
+    }
+
     
-  });
+  
+ 
 
-  one.addEventListener("click", () => {
-    contents.style.fontSize = "30px";
-    contents.style.marginRight = "10px";
-    contents.textContent = "1";
-  });
-  
-  two.addEventListener("click", () => {
-    contents.style.fontSize = "30px";
-    contents.style.marginRight = "10px";
-    contents.textContent = "2";
-  });
-  
-  three.addEventListener("click", () => {
-    contents.style.fontSize = "30px";
-    contents.style.marginRight = "10px";
-    contents.textContent = "3";
-  });
+});
+
+
 
 
